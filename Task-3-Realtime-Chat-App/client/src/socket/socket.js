950de2bae478dd3ea77e8d36846ed83587a54618
Krishna Garg <gargkrishna9354@gmail.com>
2026-08-03 11:10:25 +0530

    chore: update socket URL for production

--- Task-3-Realtime-Chat-App/client/src/socket/socket.js
+++ Task-3-Realtime-Chat-App/client/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://link-sync-backend.onrender.com", {
   autoConnect: false,
   reconnection: true,
   reconnectionAttempts: Infinity,
--- Task-3-Realtime-Chat-App/server/server.js
+++ Task-3-Realtime-Chat-App/server/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import dotenv from "dotenv";
 import { Server } from "socket.io";
 import { socketHandler } from "./socket/socketHandler.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -13,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
@@ -24,6 +27,8 @@ app.get("/", (req, res) => {
 
 socketHandler(io);
 
-server.listen(5000, () => {
-  console.log("🚀 Server Running on Port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server Running on Port ${PORT}`);
 });
